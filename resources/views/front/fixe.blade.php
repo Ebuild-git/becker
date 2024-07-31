9eda81231b8d5fe3a4c3c159480d8cc9a7bf1df8
TURBOSOFT <achref.hamouda@e-build.tn>
2024-07-31 16:38:18 +0200

    update

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -706,17 +706,17 @@
         <!-- ============================================================== -->
         <!-- All Jquery -->
         <!-- ============================================================== -->
-        <script src="./assets/js/jquery.min.js"></script>
-        <script src="./assets/js/popper.min.js"></script>
-        <script src="./assets/js/bootstrap.min.js"></script>
-        <script src="./assets/js/ion.rangeSlider.min.js"></script>
-        <script src="./assets/js/slick.js"></script>
-        <script src="./assets/js/slider-bg.js"></script>
-        <script src="./assets/js/lightbox.js"></script>
-        <script src="./assets/js/smoothproducts.js"></script>
-        <script src="./assets/js/snackbar.min.js"></script>
-        <script src="./assets/js/jQuery.style.switcher.js"></script>
-        <script src="./assets/js/custom.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/popper.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/ion.rangeSlider.min.js"></script>
+        <script src="/assets/js/slick.js"></script>
+        <script src="/assets/js/slider-bg.js"></script>
+        <script src="/assets/js/lightbox.js"></script>
+        <script src="/assets/js/smoothproducts.js"></script>
+        <script src="/assets/js/snackbar.min.js"></script>
+        <script src="/assets/js/jQuery.style.switcher.js"></script>
+        <script src="/assets/js/custom.js"></script>
         <!-- ============================================================== -->
         <!-- This page plugins -->
         <!-- ============================================================== -->
